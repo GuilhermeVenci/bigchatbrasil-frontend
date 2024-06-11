b87d3a01889959f9e73af32e34d22fc8f324c05b
Guilherme Venci <guilhermesanches.av@gmail.com>
2024-06-11 17:33:16 -0300

    Redesign Auth pages / Adjust size component Button

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,33 @@
+'use client';
 import React, { ReactNode } from 'react';
 import Image from 'next/image';
+import { BackgroundBeams } from '@/components/custom/background-beams';
+import Lottie from 'lottie-react';
+import SendAnimation from '@/assets/send-animation.json';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-1 flex flex-col lg:flex-row">
-        <div className="max-lg:h-[15vh] flex lg:flex-1 bg-gray-200 relative items-center justify-center">
-          <Image
-            src="/images/bg-auth.jpg"
-            alt="example image"
-            fill
-            sizes="100%"
-            style={{ objectFit: 'cover' }}
-            priority
+        <div className="max-lg:h-[15vh] flex lg:flex-1 bg-green-500/50 relative items-center justify-center">
+          <BackgroundBeams />
+          <Lottie
+            animationData={SendAnimation}
+            style={{ width: '100%', maxWidth: '70vw' }}
           />
         </div>
         <div className="flex-1 flex items-center justify-center bg-white max-sm:rounded-t-xl max-sm:-mt-[3vh] max-sm:z-10 max-sm:shadow-2xl">
           <div className="container px-4 pb-6 max-w-sm lg:max-w-none">
             <div className="flex flex-col items-center max-sm:-mt-[12vh]">
-              {children}
+              <Image
+                width={40}
+                height={40}
+                src="/brand-icon.svg"
+                alt="Icon Company"
+                className="h-32 sm:h-40 w-auto -mt-10 max-sm:-mt-4"
+                priority
+              />
+              <div className="-mt-16 max-sm:-mt-16">{children}</div>
             </div>
           </div>
         </div>
